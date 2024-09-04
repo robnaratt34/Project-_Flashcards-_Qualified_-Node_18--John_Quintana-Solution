cfc7ff82550d9b99831fa3da69ea86cf09536166
John <robnaratt34@gmail.com>
2024-09-04 12:24:08 -0600

    Lifted State

--- src/Layout/Study.js
+++ src/Layout/Study.js
@@ -5,10 +5,14 @@ import StudyCardView from "./StudyCardView"
 import StudyTooFew from "./StudyTooFew";
 
 function Study(){
+    const navigate = useNavigate();
     const {deckId} = useParams();
     const [deck, setDeck] = useState({});
     const [cards, setCards] = useState([]);
-
+    const [side, setSide] = useState("front")
+    const [id, setId] = useState(1)
+    const [cardId, setCardId] = useState(0)
+    
      useEffect(() => {
           async function fetchData() {
               const abortController = new AbortController();
@@ -29,11 +33,33 @@ function Study(){
           fetchData();
      }, [deckId]);
 
+    
+
+     const nextCard = ()=>{
+        if (id>=cards.length){
+            if(
+                window.confirm(
+                    `Restart Cards? \n\n Click 'cancel' to return home` 
+                )
+            ){
+                setId(1)
+                setCardId(0)
+                setSide("front")
+            }else{
+                navigate('/')
+            }
+        }else{
+            setId((id)=>id+1)
+            setCardId(id)
+            setSide("front")
+        }
+    }
+
   return(
     <div>
         <p><Link to="/">Home</Link> / <Link to={`/decks/${deckId}`}>{deck.name}</Link> / Study</p>
         <h1>{deck.name}: Study</h1>
-        {cards.length>2 ? <StudyCardView cards={cards}/> : <StudyTooFew count={cards.length} />  }
+        {cards.length>2 ? <StudyCardView cards={cards} nextCard={nextCard} side={side} setSide={setSide} cardId={cardId} id={id}/> : <StudyTooFew count={cards.length} />  }
     </div>
   )
 }
--- src/Layout/StudyCardView.js
+++ src/Layout/StudyCardView.js
@@ -1,35 +1,13 @@
-import React, {useState} from "react";
-import {useNavigate, useParams} from "react-router-dom";
+import React from "react";
 
-function StudyCardView({cards}){
-    const navigate = useNavigate();
-    const [side, setSide] = useState("front")
-    const [id, setId] = useState(1)
-    const [currentCard, setCurrentCard] = useState(cards[id-1])
-    const nextCard = ()=>{
-        if (id>=cards.length){
-            if(
-                window.confirm(
-                    `Restart Cards? \n\n Click 'cancel' to return home` 
-                )
-            ){
-                setId(1)
-                setCurrentCard(cards[0])
-                setSide("front")
-            }else{
-                navigate('/')
-            }
-        }else{
-            setId((id)=>id+1)
-            setCurrentCard(cards[id])
-            setSide("front")
-        }
-    }
+function StudyCardView(props){
+    const {cards, cardId, id, side, setSide, nextCard} = props    
+    
     if (side==="front"){
         return(
             <div>
                 <h3>Card {id} of {cards.length}</h3>
-                <p>{currentCard.front}</p>
+                <p>{cards[cardId].front}</p>
                 <button onClick={()=>setSide("back")}>Flip</button>
             </div>
         )
@@ -37,7 +15,7 @@ function StudyCardView({cards}){
         return(
             <div>
                 <h3>Card {id} of {cards.length}</h3>
-                <p>{currentCard.back}</p>
+                <p>{cards[cardId].back}</p>
                 <button onClick={()=>setSide("front")}>Flip</button>
                 <button onClick={nextCard}>Next Card</button>
             </div>
